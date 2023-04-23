1fe4dccfefd534d4c1707a5b1a3cd7f1f3216521
Lucas Hildever <lucaspaulo_212@hotmail.com>
2023-04-23 04:43:38 -0300

    project template created

---
+++ src/components/projects/projectTemplate.tsx
@@ -0,0 +1,39 @@
+import './projectsTemplate.scss'
+import arrow from '../../assets/dark-arrow.svg'
+import { FC } from 'react'
+
+interface ProjectTemplateProps {
+    title: string,
+    desc: string,
+    img: string,
+    live: string,
+    code: string,
+}
+
+const ProjectTemplate: FC<ProjectTemplateProps> = ({ title, desc, live, code, img }) => {
+    return (
+        <div className='project-template'>
+            <h1>{title}</h1>
+            <div className='project-img' style={{backgroundImage: `url(${img})`}}></div>
+            <p>{desc}</p>
+            <div className='project-btns'>
+                {
+                    live !== '' 
+                    && 
+                    <a className='white-btn' href={live} target='_blank' rel='noopener noreferrer' >
+                        <button>view live<img src={arrow} alt='arrow icon'/></button>
+                    </a>
+                }
+                {
+                    code !== ''
+                    &&
+                    <a className='white-btn' href={code} target='_blank' rel='noopener noreferrer' >
+                        <button>view code<img src={arrow} alt='arrow icon'/></button>
+                    </a>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default ProjectTemplate
