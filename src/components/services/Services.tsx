a6e76447e35cb5b217e42d7f45460b7919b400ef
Lucas Hildever <lucaspaulo_212@hotmail.com>
2023-04-23 04:37:32 -0300

    services page content and styles added

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -1,6 +1,16 @@
+import PageArrowBtn from '../shared/PageArrowBtn'
+import PageLayout from '../shared/PageLayout'
+import './services.scss'
+
 const Services = () => {
     return (
-        <h1>services</h1>
+        <PageLayout className='services-page'>
+            <h2>Creative Web Solutions <br/>& Outstanding Design</h2>
+            <p>Website developed for coint co, an ecommerce focused on selling collectible metal/physic bitcoins Website developed for coint co, an ecommerce focused on selling collectible metal/physic bitcoins.</p>
+            <PageArrowBtn to='/projects'>
+                view projects
+            </PageArrowBtn>
+        </PageLayout>
     )
 }
 
