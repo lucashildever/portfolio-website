479fe6c72e48d517e069c229c704c05e3be78f7e
Lucas Hildever <lucaspaulo_212@hotmail.com>
2023-04-23 04:38:01 -0300

    about section icon added

--- src/components/home/about-section/AboutSection.tsx
+++ src/components/home/about-section/AboutSection.tsx
@@ -4,7 +4,11 @@ import Button from '../../shared/Button'
 const AboutSection = () => {
     return (
         <div className='about-section'>
-            <div>
+            <span className='spin-icon'>
+                <div className='st'></div>
+                <div className='nd'></div>
+            </span>
+            <div className='about-sec-content'>
                 <h2>Hello,<br/> i'm Lucas<br/> Hildever</h2>
                 <Button to='/about' className='white-btn'>know more</Button>
             </div>
