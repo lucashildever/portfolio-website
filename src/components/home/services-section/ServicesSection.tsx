04c7431494d6bd9837cf92bf8d44f200f88f2447
Lucas Hildever <lucaspaulo_212@hotmail.com>
2023-04-23 04:43:20 -0300

    services section and projects updates

--- src/components/home/services-section/ServicesSection.tsx
+++ src/components/home/services-section/ServicesSection.tsx
@@ -4,8 +4,8 @@ import './services-section.scss'
 const ServicesSection = () => {
     return (
         <div className='services-section'>
-            <div>
-                <h2>Creative Web solutions</h2>
+            <div className='services-sec-content'>
+                <h2>Creative Web solutions & </h2>
                 <Button to='/services' className='white-btn'>services</Button>
             </div>
         </div>
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,6 +1,30 @@
+import type { RootState } from '../../states/store'
+import { useSelector } from 'react-redux'
+
+import PageLayout from '../shared/PageLayout'
+import ProjectTemplate from './projectTemplate'
+import './projects.scss'
+
 const Projects = () => {
+
+    const projects = useSelector((state: RootState) => state.projects.value)
+    console.log(projects)
+
     return (
-        <h1>projects</h1>
+        <PageLayout className='projects-page'>
+            {
+                projects
+                    .map(item => 
+                        <ProjectTemplate
+                            title={item.title}
+                            desc={item.desc}
+                            live={item.live}
+                            code={item.code}
+                            img={item.img}
+                        />
+                    )
+            }
+        </PageLayout>
     )
 }
 
