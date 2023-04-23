feb897141ddc585aa20e68ed6a54c7c61f47e5d8
Lucas Hildever <lucaspaulo_212@hotmail.com>
2023-04-23 04:42:32 -0300

    button component props update

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -1,12 +1,12 @@
+import { PropsWithChildren } from 'react'
 import { Link } from 'react-router-dom'
 
-interface ButtonProps {
+interface ButtonProps  {
     to: string,
     className?: string,
-    children: React.ReactNode
 }
 
-const Button = ({ to, className, children }: ButtonProps) => {
+const Button = ({ to, className, children }: PropsWithChildren<ButtonProps>) => {
     return (
         <Link className={className} to={to}>
             <button>{children}</button>
