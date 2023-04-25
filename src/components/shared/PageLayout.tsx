74f546675b51f7b3f4a9645333952f68c20d6c5b
Lucas Hildever <lucaspaulo_212@hotmail.com>
2023-04-25 18:01:34 -0300

    page layout update

--- src/components/shared/PageLayout.tsx
+++ src/components/shared/PageLayout.tsx
@@ -1,3 +1,5 @@
+import './page-layout.scss'
+
 import { PropsWithChildren } from 'react'
 import Button from './Button'
 import arrow from '../../assets/arrow.svg'
@@ -5,9 +7,11 @@ import arrow from '../../assets/arrow.svg'
 const PageLayout = ({children, className}: PropsWithChildren<{className?: string}>) => {
     return (
         <div className={className}>
-            <Button to='/' className='arrow-btn'>
-                <img src={arrow} alt='arrow icon'/>home
-            </Button>
+            <div className='page-layout-btn'>
+                <Button to='/' className='arrow-btn'>
+                    <img src={arrow} alt='arrow icon'/>home
+                </Button>
+            </div>
             {children}
         </div>
     )
